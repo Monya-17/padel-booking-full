371651eda35b853697f166dcfeaa43dd89e59c16
Monya ALShuhoumi <mmmmonya94@gmail.com>
2026-08-16 01:50:55 +0400

    Enhance JSON serialization with enum converter
    Added JSON options to include string enum converter.

--- backend/PadelBooking.Api/Program.cs
+++ backend/PadelBooking.Api/Program.cs
@@ -9,7 +9,11 @@ using PadelBooking.Api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // ---- Services ----
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
